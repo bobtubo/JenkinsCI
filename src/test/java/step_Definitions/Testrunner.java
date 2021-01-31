d1ae4500994404c24198259658809e498a3a2e28
unknown <bobtubo>
2021-01-31 18:27:05 +0000

    latest update

--- src/test/java/step_Definitions/Testrunner.java
+++ src/test/java/step_Definitions/Testrunner.java
@@ -5,6 +5,17 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\Bobby\\IdeaProjects\\uiFunctional\\src\\main\\resources\\feature_files", plugin = {"pretty", "html:target/Myreport"}, tags = "@Test")
+@CucumberOptions(features = "C:\\Users\\Bobby\\IdeaProjects\\uiFunctional\\src\\main\\resources\\feature_files",
+//        plugin = {"pretty", "html:target/Myreport"}, tags = "@Test")
+
+        monochrome = true,
+        plugin = {
+                "pretty",
+                "html:target/cucumber.html",
+                "json:target/cucumber/cucumber-report.json",
+                "junit:target/cucumber.xml"
+        }, tags="@Test")
+
+
 public class Testrunner {
 }
